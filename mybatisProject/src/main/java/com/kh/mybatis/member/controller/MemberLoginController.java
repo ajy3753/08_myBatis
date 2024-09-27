20ca12efe0c188618fe6e391d9b0aa0fa2086df1
ajy3753 <ajy3753@gmail.com>
2024-09-27 16:30:01 +0900

    first commit

---
+++ mybatisProject/src/main/java/com/kh/mybatis/member/controller/MemberLoginController.java
@@ -0,0 +1,54 @@
+package com.kh.mybatis.member.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import com.kh.mybatis.member.model.vo.Member;
+import com.kh.mybatis.member.service.MemberServiceImpl;
+
+/**
+ * Servlet implementation class MemberLoginController
+ */
+public class MemberLoginController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MemberLoginController() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		
+		Member m = new Member();
+		m.setUserId(request.getParameter("userId"));
+		m.setUserPwd(request.getParameter("userPwd"));
+		
+		Member loginUser = new MemberServiceImpl().loginMember(m);
+		
+		if(loginUser != null) {
+			request.getSession().setAttribute("loginUser", loginUser);
+			response.sendRedirect(request.getContextPath());
+		} else {
+			request.setAttribute("errorMsg", "로그인 실패");
+			request.getRequestDispatcher("views/common/errorPage.jsp").forward(request, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ mybatisProject/src/main/java/com/kh/mybatis/member/service/MemberServiceImpl.java
@@ -0,0 +1,42 @@
+package com.kh.mybatis.member.service;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.kh.mybatis.common.template.Template;
+import com.kh.mybatis.member.model.dao.MemberDao;
+import com.kh.mybatis.member.model.vo.Member;
+
+public class MemberServiceImpl implements MemberService {
+	private MemberDao mDao = new MemberDao();
+	
+	@Override
+	public Member loginMember(Member m) {
+//		Connection conn = getConnecntion();
+//		
+//		Member loginUser = new MemberDao().loginMember(conn, m);
+//		close(conn);
+//		
+//		return loginUser;
+		
+		SqlSession sqlSession = Template.getSqlSession();
+		Member loginUser = mDao.loginMember(sqlSession, m);
+		sqlSession.close();
+		
+		return loginUser;
+	}
+
+	@Override
+	public int insertMember(Member m) {
+		SqlSession sqlSession = Template.getSqlSession();
+		
+		int result = mDao.insertMember(sqlSession, m);
+		if(result > 0) {
+			sqlSession.commit();
+		}
+		else {
+			sqlSession.rollback();
+		}
+		
+		return result;
+	}
+}
